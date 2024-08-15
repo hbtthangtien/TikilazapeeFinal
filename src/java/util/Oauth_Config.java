52b468419d818064cef3e27aeafad825554f880c
tungbdhe170285 <hbtthangtien@gmail.com>
2024-08-15 13:29:42 +0700

    uncomment google api:

---
+++ src/java/util/Oauth_Config.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author hbtth
+ */
+public class Oauth_Config {
+
+    public static String FACEBOOK_APP_ID = "3757548414488800";
+    public static String FACEBOOK_APP_SECRET = "1aa21ec33261e33d82415aa220fe8ec6";
+    public static String FACEBOOK_REDIRECT_URL = "http://localhost:9999/tikilazapee/facebook-handler";
+    public static String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&redirect_uri=%s&code=%s";
+    public static String FACEBOOK_LINK_GET_CODE = "https://www.facebook.com/v20.0/dialog/oauth?client_id=3757548414488800&redirect_uri=http://localhost:9999/tikilazapee/facebook-handler";
+    //public static String GOOGLE_CLIENT_ID = "330684834215-1c16ij7qkd7d7s9mbac1p4jmoi5f7m7j.apps.googleusercontent.com";
+    //public static String GOOGLE_APP_SERCRET = "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";
+    //public static String GOOGLE_REDIRECT_URL = "http://localhost:9999/tikilazapee/google-handler";
+    //public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    //public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    //public static String GOOGLE_LINK_GET_CODE = "https://accounts.google.com/o/oauth2/auth?scope=profile"
+    //        + "&redirect_uri=http://localhost:9999/tikilazapee/google-handler"
+    //        + "&response_type=code&client_id=330684834215-1c16ij7qkd7d7s9mbac1p4jmoi5f7m7j.apps.googleusercontent.com&approval_prompt=force";
+   // public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}   
