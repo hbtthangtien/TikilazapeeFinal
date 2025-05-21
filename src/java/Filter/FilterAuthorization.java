e3e90fb4025824f5f4daf36bd6228af4e6264130
Duc Tung <hbtthangtien@gmail.com>
2025-05-21 22:14:39 +0700

    commit

--- src/java/Filter/FilterAuthorization.java
+++ src/java/Filter/FilterAuthorization.java
@@ -69,36 +69,9 @@ public class FilterAuthorization implements Filter {
             throws IOException, ServletException {
         if (debug) {
             log("FilterAuthorization:DoAfterProcessing");
-        }
-
-        // Write code here to process the request and/or response after
-        // the rest of the filter chain is invoked.
-        // For example, a logging filter might log the attributes on the
-        // request object after the request has been processed. 
-        /*
-	for (Enumeration en = request.getAttributeNames(); en.hasMoreElements(); ) {
-	    String name = (String)en.nextElement();
-	    Object value = request.getAttribute(name);
-	    log("attribute: " + name + "=" + value.toString());
-
-	}
-         */
-        // For example, a filter might append something to the response.
-        /*
-	PrintWriter respOut = new PrintWriter(response.getWriter());
-	respOut.println("<P><B>This has been appended by an intrusive filter.</B>");
-         */
+        }        
     }
 
-    /**
-     *
-     * @param request The servlet request we are processing
-     * @param response The servlet response we are creating
-     * @param chain The filter chain we are processing
-     *
-     * @exception IOException if an input/output error occurs
-     * @exception ServletException if a servlet error occurs
-     */
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
@@ -106,7 +79,7 @@ public class FilterAuthorization implements Filter {
         if (debug) {
             log("FilterAuthorization:doFilter()");
         }
-
+        
         doBeforeProcessing(request, response);
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
@@ -158,6 +131,7 @@ public class FilterAuthorization implements Filter {
         Throwable problem = null;
         try {
             chain.doFilter(request, response);
+            
         } catch (Throwable t) {
             // If an exception is thrown somewhere down the filter chain,
             // we still want to execute our after processing, and then
@@ -197,9 +171,7 @@ public class FilterAuthorization implements Filter {
         this.filterConfig = filterConfig;
     }
 
-    /**
-     * Destroy method for this filter
-     */
+   
     public void destroy() {
     }
 
